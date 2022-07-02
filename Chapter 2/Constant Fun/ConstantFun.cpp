6f29ad5735a65601f5e54592f25a9d7243b012e3
Mohammadreza Esmaeillou <51795847+MohammadEsmaeillou@users.noreply.github.com>
2022-07-02 14:35:40 -0700

    Add files via upload

---
+++ Chapter 2/Constant Fun/ConstantFun.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    //Variables typically use camel casing
+    //for constants we use captilized snake casing
+
+    const double MY_PI = 3.14159;
+    int myTaxRate = 4;
+    const string MY_NAME = "Moe";
+
+    cout << MY_NAME << endl;
+
+    return 0;
+}
