9ba5499149dbada7e12ad58a88cd5fe1d8c323fe
Mohammadreza Esmaeillou <51795847+MohammadEsmaeillou@users.noreply.github.com>
2022-07-02 14:36:44 -0700

    Add files via upload

---
+++ Chapter 2/Change is Nice/ChangeIsNice.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int myNum =10;
+    cout << myNum << endl;
+    myNum = myNum + 5;
+    cout << myNum << endl;
+    myNum = myNum * 2;
+    cout << myNum << endl;
+    myNum = myNum * 2;
+    cout << myNum << endl;
+    myNum = myNum * 2;
+    cout << myNum << endl;
+
+    //Second Method
+    myNum = 10;
+    cout << myNum << endl;
+    myNum +=5;
+    cout << myNum << endl;
+    myNum *= 2;
+    cout << myNum << endl;
+    myNum *= 2;
+    cout << myNum << endl;
+    myNum *= 2;
+    cout << myNum << endl;
+
+}
