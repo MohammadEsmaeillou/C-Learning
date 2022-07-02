fdeb020380c3652d0e5788eb91e87304b7743dca
Mohammadreza Esmaeillou <51795847+MohammadEsmaeillou@users.noreply.github.com>
2022-07-02 14:36:13 -0700

    Add files via upload

---
+++ Chapter 2/Comment Fun/CommentFun.cpp
@@ -0,0 +1,16 @@
+/*
+    The Complete C++ Developer Course
+    Comments Class
+    Instructor: Dr. John P. Baugh
+    Student: Mohammadreza (Moe) Esmaeillou
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 30; //This is my current age
+    double tax = 0.06; //sales tax in michigan
+    return 0;
+}
