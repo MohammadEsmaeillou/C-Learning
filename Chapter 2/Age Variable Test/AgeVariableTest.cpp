52d7dabe95cf9a3b12cc597117a33ccb7f2b9e04
Mohammadreza Esmaeillou <51795847+MohammadEsmaeillou@users.noreply.github.com>
2022-07-02 14:40:03 -0700

    Add files via upload

---
+++ Chapter 2/Age Variable Test/AgeVariableTest.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << boolalpha;
+    int myAge = 23;
+    bool drinkingAge = myAge >= 21;
+    cout << "Age >= 21? : " << drinkingAge << endl;
+}
