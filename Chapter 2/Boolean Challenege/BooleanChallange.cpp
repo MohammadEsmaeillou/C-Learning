99445812c7cda902856399b39e9f3d98fa51ea8f
Mohammadreza Esmaeillou <51795847+MohammadEsmaeillou@users.noreply.github.com>
2022-07-02 14:38:15 -0700

    Add files via upload

---
+++ Chapter 2/Boolean Challenege/BooleanChallange.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool sunny = true;
+    bool warm = true;
+
+    cout << boolalpha << sunny << endl;
+    cout << warm << endl;
+
+    return 0;
+}
