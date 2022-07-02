faa050e07eee54f8ca2c4c7646d30bdd075e1b68
Mohammadreza Esmaeillou <51795847+MohammadEsmaeillou@users.noreply.github.com>
2022-07-02 14:30:33 -0700

    Add files via upload

---
+++ Chapter 2/Text Fun/TextFun.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    char singleChar = 'a';
+    cout << singleChar << endl;
+
+    string myName = "Moe";
+    cout << myName << endl;
+
+    //adding operations in cout
+    cout << "Hello " << myName << endl;
+    return 0;
+}
