fcc32e499f8cc913c97da7d5f0dc07304bb8ebff
Mohammadreza Esmaeillou <51795847+MohammadEsmaeillou@users.noreply.github.com>
2022-07-02 14:37:31 -0700

    Add files via upload

---
+++ Chapter 2/Boolean Fun/BooleanFun.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    bool rain = false;
+    //boolalpha will print out true/false
+    cout<< boolalpha << rain <<endl;
+
+    return 0;
+}
