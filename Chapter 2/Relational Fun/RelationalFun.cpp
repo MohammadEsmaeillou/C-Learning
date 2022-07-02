2975a07784a58d05d65f9f312580d6188ae01bcb
Mohammadreza Esmaeillou <51795847+MohammadEsmaeillou@users.noreply.github.com>
2022-07-02 14:31:18 -0700

    Add files via upload

---
+++ Chapter 2/Relational Fun/RelationalFun.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << boolalpha;
+
+    int a = 15;
+    int b = 20;
+    bool areEqual = a ==b;
+
+    cout << ( a < b) << endl;
+    cout << areEqual << endl;
+
+    return 0;
+}
